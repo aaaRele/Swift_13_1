7f8b6a98d32c93409eec308283eeade0bf81a98a
WYF <1714436520@qq.com>
2018-12-18 12:02:10 +0800

    2016110339_wyf

---
+++ 13-1/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  13-1
+//
+//  Created by student on 2018/12/15.
+//  Copyright © 2018年 2016110339. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sumLable: UILabel!
+    @IBOutlet weak var countLable: UILabel!
+    var count = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+            self.count += 1
+            print("Timer Thread;\(Thread.current)")
+            self.countLable.text = "\(self.count)"
+        }
+    }
+    
+    @IBAction func sum(_ sender: Any) {
+        var sum = 0
+        DispatchQueue.global().async {
+            print("Sum Thread;\(Thread.current)")
+            for i in 1...9999999{
+                sum += i
+            }
+            DispatchQueue.main.async {
+                self.sumLable.text = "\(sum)"
+                
+            }
+        }
+
+    }
+    
+}
+
